a2c889530012a1e358c09d83a56e3b08cc1ff174
Lucas Patricio <lucas_patricio@outlook.com.br>
2021-04-26 23:05:52 -0300

    Adicionando projeto base IO

---
+++ ByteBankImportacaoExportacao/LidandoComFileStream.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ByteBankImportacaoExportacao
+{
+    partial class Program
+    {
+        public static void LidandoComFileStreamDiretamente() {
+            string localArquivo = "contas.txt";
+
+            using (var fluxoDoArquivo = new FileStream(localArquivo, FileMode.Open))
+            {
+                byte[] buffer = new byte[1024]; //1kb
+                int numeroRetornado = -1;
+
+
+                while (numeroRetornado != 0)
+                {
+                    numeroRetornado = fluxoDoArquivo.Read(buffer, 0, 1024);
+                    MostrarNaTela(buffer, numeroRetornado);
+                }
+            }
+        }
+        public static void MostrarNaTela(byte[] buffer, int bytesLidos)
+        {
+            var utf8 = new UTF8Encoding();
+            string texto = utf8.GetString(buffer, 0, bytesLidos);
+
+            Console.WriteLine(texto);
+
+            //foreach(var meuByte in buffer)
+            //{
+            //    Console.Write(meuByte);
+            //    Console.Write(" ");
+            //}
+        }
+
+    }
+}
---
+++ ByteBankImportacaoExportacao/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ByteBankImportacaoExportacao
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            string localArquivo = "contas.txt";
+
+            using (var fluxoDeArquivos = new FileStream(localArquivo, FileMode.Open))
+            using (var leitor = new StreamReader(fluxoDeArquivos))
+            {
+                while (!leitor.EndOfStream)
+                {
+                    string linha = leitor.ReadLine();
+                    Console.WriteLine(linha);
+                }
+            }
+        }
+    }
+}
